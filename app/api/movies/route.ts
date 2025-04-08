380625db368472582efca6659378331e099e3d39
Rgar <rgar@gmail.com>
2025-04-08 16:29:23 +0200

    WIP

---
+++ app/api/movies/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+import { Db, MongoClient } from 'mongodb';
+import clientPromise from '@/lib/mongodb';
+
+export async function GET(): Promise<NextResponse> {
+  try {
+    const client: MongoClient = await clientPromise;
+    const db: Db = client.db('sample_mflix');
+    const movies = await db.collection('movies').find({}).limit(10).toArray();
+    
+    return NextResponse.json(
+	    { status: 200, data: movies }
+		);
+  }
+  catch (error: any) {
+    return NextResponse.json(
+	    { status: 500, message: 'Internal Server Error', error: error.message }
+    );
+  }
+}
